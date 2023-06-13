d92ef479d4b50c7dd8b212cf39a67704927f33e5
JohnAFernandez <jafernandez.tampabay@gmail.com>
2023-06-12 22:04:08 -0400

    just testing rust out

---
+++ Rust Trader/rust_trader/src/main.rs
@@ -0,0 +1,18 @@
+
+
+
+fn main() {
+    let bob = 1;
+
+    
+    println!("Hello, world! bob");
+
+    let mut bob_string = std::string("I'm a teapot.");
+
+    println!(bob_string);
+
+    bob_string.clear();
+
+    println!(bob_string);
+
+}
