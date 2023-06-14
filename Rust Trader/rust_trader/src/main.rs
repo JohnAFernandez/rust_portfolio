f20f749801efa16f5defb70f7ef3553558f2642a
JohnAFernandez <jafernandez.tampabay@gmail.com>
2023-06-14 00:35:50 -0400

    Learning a new language is easy....

--- Rust Trader/rust_trader/src/main.rs
+++ Rust Trader/rust_trader/src/main.rs
@@ -1,18 +1,41 @@
+struct ShipType {
+    cargo_volume : f32,
+    max_cargo_volume : f32,
+
+    current_crew : i16,
+    crew_minimum : i16,
+    crew_maximum : i16,
+    
+    passengers: i16, // max determined by cargo maximum
+    base_mass: f64,
+    current_mass: f64,
 
 
+    name : str
+    
+}
+
+
+struct Player {
+    credits : i64,
+    reputation : i16,
+    morality : i16,
+    current_ship : ShipType
+}
+
+
+fn start_game() {
+    let mut fox = Shuo
+
+
+    player : Player(1000, 0, 0, ) // lol, I need to know how to instantiate things.
+}
 
 fn main() {
-    let bob = 1;
 
-    
-    println!("Hello, world! bob");
+    // where's my game state?
 
-    let mut bob_string = std::string("I'm a teapot.");
+    // WHERE IS MAI GAME STATE!!
 
-    println!(bob_string);
-
-    bob_string.clear();
-
-    println!(bob_string);
-
+    start_game()
 }
